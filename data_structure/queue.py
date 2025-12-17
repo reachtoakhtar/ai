f4e993e4a9c90e3183b1615917dc16e57f82846c
Akhtar <reachtoakhtar@gmail.com>
2025-12-17 18:07:27 +0530

    Queue.

---
+++ data_structure/queue.py
@@ -0,0 +1,27 @@
+__author__ = "akhtar"
+
+import logging
+from collections import deque
+
+logger = logging.getLogger(__name__)
+
+
+class Queue:
+    def __init__(self):
+        self.buffer = deque()
+
+    def enqueue(self, val):
+        self.buffer.appendleft(val)
+
+    def dequeue(self):
+        if len(self.buffer)==0:
+            print("Queue is empty")
+            return
+
+        return self.buffer.pop()
+
+    def is_empty(self):
+        return len(self.buffer) == 0
+
+    def size(self):
+        return len(self.buffer)
